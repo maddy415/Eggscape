3a0f2d9e50d55ed6c53afbd645b6e6f1ae779b49
maddy <luis.maia@escolar.ifrn.edu.br>
2025-06-29 20:38:38 -0300

    New obstacle gen system stable
    -As patterns já são instanciadas quanto detecta colisão entre o SpawnTrigger e SpawnNextTrigger

--- Eggscape/Assets/Scripts/PatternGen.cs
+++ Eggscape/Assets/Scripts/PatternGen.cs
@@ -10,29 +10,45 @@ public class PatternGen : MonoBehaviour
     private List<GameObject> patternsList = new List<GameObject>();
     public GameObject spawnPoint;
     public GameObject SpawnTrigger;
+    public GameObject nextPatternSpawn;
     private bool spawned = false;
+    private SpawnTriggerHandler handler;
     
     //Anotações pra qnd abrir dnv:
     //Quando o spawn no SpawnTriggerHandler for true, a gnt spawna uma pattern nova por aqui
 
     void Start()
     {
+        handler = SpawnTrigger.GetComponent<SpawnTriggerHandler>();
+        nextPatternSpawn = GameObject.FindWithTag("SpawnNextTrigger");
+
         foreach (GameObject pattern in patterns)
         {
             patternsList.Add(pattern);
 
         }
 
-        StartCoroutine(WaitToSpawn());
-        SpawnTrigger.GetComponent<SpawnTriggerHandler>();
-        
+        //StartCoroutine(WaitToSpawn());
+        SpawnPattern();
 
+
+    }
+
+    void Update()
+    {
+        if (handler.TriggeredSpawn)
+        {
+            Debug.Log("spawned");
+            SpawnPattern();
+            handler.TriggeredSpawn = false;
+        }
     }
 
     void SpawnPattern()
     {
         GameObject patternClone = Instantiate(patterns[Random.Range(0, patterns.Length)], spawnPoint.transform.position, Quaternion.identity);
         Debug.Log(patternClone);
+        
     }
     
     IEnumerator WaitToSpawn()
