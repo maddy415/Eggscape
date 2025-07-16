77ffcb832d0c251acea94154b064d312c0c01d1c
maddy <luis.maia@escolar.ifrn.edu.br>
2025-07-15 21:29:45 -0300

    sla porra

---
+++ Eggscape/Assets/Scripts/CloudMove.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+public class CloudMove : MonoBehaviour
+{
+    public float moveSpeed;
+    private List<GameObject> clouds = new List<GameObject>();
+    private void Update()
+    {
+        transform.Translate(Vector3.right* moveSpeed * Time.deltaTime, Space.World);
+
+        foreach (Transform child in gameObject.transform)
+        {
+            clouds.Add(child.gameObject);
+        }
+    }
+}
